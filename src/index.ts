681f2e1d6143dc381b636f0f44bdafbc6c9618e9
webcounters <webcounters@gmail.com>
2023-03-23 03:21:46 +0300

    remove

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export default KVGrav;
 
 export { default as CoordiMass } from "./types/CoordiMass";
 export { default as Coordinate } from "./types/Coordinate";
+export { default as KvgOptionsType } from "./types/KvgOptionsType";
--- src/types/HttpOptions.ts
+++
@@ -1,6 +0,0 @@
-type HttpOptions = {
-  raw: string;
-  timeout: number | null;
-  headers: ReadonlyArray<Record<string, string>> | null
-}
-export default HttpOptions;
